1019a14ee45004c9d12fb9ed958cd47287bed1f9
hanford <jackhanford@gmail.com>
2018-05-05 13:59:38 +0200

    extend demo to cover some more props

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,16 +1,17 @@
-import React, {Component, Fragment} from 'react'
-import {render} from 'react-dom'
-import {css} from 'emotion'
+import React, { PureComponent, Fragment } from 'react'
+import GithubBadge from 'react-github-badge'
+import { render } from 'react-dom'
+import { css } from 'emotion'
 
 import Drawer from '../../src'
 
 const bigArray = new Array(500).fill(true)
 
-class Demo extends Component {
+class Demo extends PureComponent {
   state = {
     regular: false,
-    jsScroll: false,
-    jsScrollAsyncHeight: false
+    sidebar: false,
+    asyncHeight: false
   }
 
   toggle = (type, value) => event => {
@@ -29,15 +30,29 @@ class Demo extends Component {
   }
 
   render() {
-    const { regular, jsScroll, jsScrollAsyncHeight} = this.state
+    const { regular, sidebar, asyncHeight, crazyStyle } = this.state
+
     return (
-      <Fragment>
-        <div className={ButtonContainer}>
-          <button onClick={this.toggle('regular', true)} className={Toggle}>Open drawer!</button>
-          <br />
-          <br />
-          <button onClick={this.toggle('jsScrollAsyncHeight', true)} className={Toggle}>Huge drawer with async height</button>
-        </div>
+      <div className={`${Reset} ${Container}`}>
+        <GithubBadge
+          title='Star on Github'
+          url='https://github.com/hanford/react-drag-drawer'
+        />
+
+        <h1>React Drag Drawer</h1>
+        <Info>react-drag-drawer is a lightweight, performant, drawer/modal component that can be dragged close. The animations are done with react-motion so they feel very natural</Info>
+        <button onClick={this.toggle('regular', true)} className={Toggle}>Open example</button>
+        <div className={Break} />>
+        <Info>react-drag-drawer uses native HTML5 scrolling to remain performant and to properly respond to async data / components</Info>
+        <button onClick={this.toggle('asyncHeight', true)} className={Toggle}>Async height</button>
+        <div className={Break} />
+        <Info>You can also use react-drag-drawer to build sidebars by simply changing the `direction` prop</Info>
+        <button onClick={this.toggle('sidebar', true)} className={Toggle}>Sidebar</button>
+        <div className={Break} />
+        <Info>react-drag-drawer assumes nothing about your styles and is left entirely up to you how you want to style it</Info>
+        <button onClick={this.toggle('crazyStyle', true)} className={Toggle}>Different style</button>
+        <div className={Break} />
+        <Info>react-drag-drawer has a <a target='_blank' href='https://github.com/hanford/react-drag-drawer#api'>small but extensive API</a> that allows for some very cool customizations</Info>
 
         <Drawer
           open={regular}
@@ -50,17 +65,31 @@ class Demo extends Component {
           </div>
         </Drawer>
 
+        <Drawer
+          open={sidebar}
+          onRequestClose={this.toggle('sidebar', false)}
+          modalElementClass={Sidebar}
+          direction='x'
+        >
+          <div className={Card}>
+            I'm a sidebar drawer
+            <button className={Toggle} onClick={this.toggle('sidebar', false)}>Close drawer</button>
+          </div>
+        </Drawer>
+
         <AsyncHeightDrawer
-          open={jsScrollAsyncHeight}
-          onRequestClose={this.toggle('jsScrollAsyncHeight', false)}
+          open={asyncHeight}
+          onRequestClose={this.toggle('asyncHeight', false)}
           modalElementClass={HugeList}
         />
-      </Fragment>
+      </div>
     )
   }
 }
 
-class AsyncHeightDrawer extends Component {
+const Info = ({ children }) => <p className={InfoComponent}>{children}</p>
+
+class AsyncHeightDrawer extends PureComponent {
   state = {
     asyncData: []
   }
@@ -92,21 +121,28 @@ class AsyncHeightDrawer extends Component {
   }
 }
 
+const Reset = css`
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`
+
 const Content = css`
   background-color: white;
 `
 
 const Card = css`
   height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 40px auto;
 
   @media(min-width: 768px) {
-    height: 300px;
-    margin: 0 auto;
     border-radius: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
   }
 
   button {
@@ -134,8 +170,29 @@ const modal = css`
   position: absolute;
   top: 30px;
   background-color: white;
-  width: 375px;
+  width: 100%;
+  max-width: 700px;
   min-height: 100%;
+  border-top-left-radius: 4px;
+  border-top-right-radius: 4px;
+`
+
+const Sidebar = css`
+  ${modal}
+  top: 0;
+  max-width: 300px;
+  border-radius: 0;
+  left: 0;
+`
+
+const CrazyStyle = css`
+  ${modal}
+  top: auto;
+  min-height: 100%;
+  width: 100%;
+  max-width: 100%;
+  display: flex;
+  align-items: center;
 `
 
 const ModalElement = css`
@@ -150,16 +207,28 @@ const ModalElement = css`
 const HugeList = css`
   ${modal}
   text-align: center;
+  overflow: auto;
 
   @media(max-width: 767px) {
     width: 100%;
   }
 `
 
-const ButtonContainer = css`
+const Container = css`
   margin: 200px auto 0;
   width: 100%;
-  text-align: center;
+
+  font-family: arial;
+  max-width: 600px;
+`
+
+const Break = css`
+  width: 100%;
+  margin-bottom: 20px;
+`
+
+const InfoComponent = css`
+
 `
 
 render(<Demo/>, document.querySelector('#demo'))
